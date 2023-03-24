2abfb4aa0de4d7f3948689466c2f4c977d49962b
*Khamzat <*backslider369@gmail.com*>
2023-03-24 22:37:23 +0300

    Initial commit

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,13 @@
+package web.service;
+
+import web.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> findAllUser();
+    User findUserById(int id);
+    void saveUserByUser(User user);
+    void updateUserByUserAndId(User user, int id);
+    void deleteUserById(int id);
+}
